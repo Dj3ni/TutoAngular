2ff8e7fdd77c3307bb917cf5b31d0520ffae3a54
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-02-28 15:15:13 +0100

    input card

--- playing-cards/src/app/components/playing-card/playing-card.component.ts
+++ playing-cards/src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Monster } from '../../models/monster.model';
+
 
 @Component({
   selector: 'app-playing-card',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './playing-card.component.css'
 })
 export class PlayingCardComponent {
-    imageUrl : any ="/img/energyIcons/electric.png"
+    imageUrl : any ="/img/energyIcons/electric.png";
+
+    //En faisant cela on les transforme en attributs
+    @Input({
+      transform:(value:Monster) => {
+        value.hp = value.hp/2
+        return value;
+      }
+    })
+    public monster : Monster = new Monster();
+
 }
---
+++ playing-cards/src/app/models/monster.model.ts
@@ -0,0 +1,8 @@
+export class Monster {
+    name : string = 'Monster';
+    hp : number = 40;
+    figureCaption : string = "N°001 Monster";
+    attackName : string = "GeoImpact";
+    attackStrenght : number = 60;
+    attackDescription : string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugiat libero eveniet eos sit alias."
+}
