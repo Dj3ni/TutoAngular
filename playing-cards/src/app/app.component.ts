4f6e42ab80ed7ce07b3aff83402cec59599d2d85
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-02-28 14:05:43 +0100

    card component

--- playing-cards/src/app/app.component.ts
+++ playing-cards/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // imports: [RouterOutlet],
+  imports:[PlayingCardComponent],
+  templateUrl: './app.component.html', // lien vers feuille html
+  styleUrl: './app.component.css' // lien vers feuille de style
 })
 export class AppComponent {
   title = 'playing-cards';
--- playing-cards/src/app/app.routes.ts
+++ playing-cards/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path:'playing-card', component:PlayingCardComponent}
+];
