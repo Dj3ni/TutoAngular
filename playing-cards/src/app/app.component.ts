a609249ef37c5ce6c639f84ba5db84c7efc60208
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-03-01 18:17:21 +0100

    @ if et @ for

--- playing-cards/src/app/app.component.ts
+++ playing-cards/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, model, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
@@ -20,10 +20,15 @@ export class AppComponent {
   // search : string = '';
 
   public monsters! : Monster[];
-  monster1! : Monster;
-  monster2! : Monster;
-  monster3! : Monster;
-  monster4! : Monster;
+  public monster1! : Monster;
+  public monster2! : Monster;
+  public monster3! : Monster;
+  public monster4! : Monster;
+
+  search = model('');
+  filteredMonsters = computed(()=>{
+    return this.monsters.filter(m =>m.name.includes(this.search()))
+  })
 
     constructor(){
       // utilisation signal effect()
@@ -37,7 +42,7 @@ export class AppComponent {
       this.monsters = [];
       // Init Pikachu
     this.monster1 = new Monster();
-      this.monster1.name = "Pikachu";
+      this.monster1.name = "pikachu";
       this.monster1.hp = 40;
       this.monster1.figureCaption ='N°002 Pikachu';
       this.monster1.type = MonsterType.ELECTRIC;
@@ -45,21 +50,21 @@ export class AppComponent {
     this.monsters.push(this.monster1);
     // Init Carapuce
     this.monster2 = new Monster();
-      this.monster2.name = "Carapuce";
+      this.monster2.name = "carapuce";
       this.monster2.hp = 60;
       this.monster2.type = MonsterType.WATER;
       this.monster2.image = 'img/cards/carapuce.png';
       this.monster2.figureCaption ='N°003 Carapuce'
       this.monsters.push(this.monster2)
     this.monster3 = new Monster();
-      this.monster3.name = "Osselets";
+      this.monster3.name = "osselets";
       this.monster3.hp = 70;
       this.monster3.type = MonsterType.PLANT;
       this.monster3.image = 'img/cards/osselet.png';
       this.monster3.figureCaption ='N°004 Osselet'
       this.monsters.push(this.monster3)
     this.monster4 = new Monster();
-      this.monster4.name = "Evoli";
+      this.monster4.name = "evoli";
       this.monster4.hp = 50;
       this.monster4.type = MonsterType.FIRE;
       this.monster4.image = 'img/cards/evoli.png';
