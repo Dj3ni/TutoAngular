94898d7cf898dd9487967f7fba6f9dbb5b49ddb0
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-03-23 17:19:54 +0100

    init login component + route

--- playing-cards/src/app/app.routes.ts
+++ playing-cards/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { PlayingCardComponent } from './components/playing-card/playing-card.com
 import { MonsterListComponent } from './pages/monster-list/monster-list.component';
 import { MonsterComponent } from './pages/monster/monster.component';
 import { MonsterNotFoundComponent } from './pages/monster-not-found/monster-not-found.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'home', component : MonsterListComponent},
+    {path:"login", component: LoginComponent},
     {path : 'monster', children : [
         {path:'', component : MonsterComponent,},
         {path:':id', component : MonsterComponent},
---
+++ playing-cards/src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  private _formBuilder = inject(FormBuilder);
+
+  loginFormGroup = this._formBuilder.group({
+    'username': ['', [Validators.required]],
+    'password': ['',[Validators.required]]
+  });
+
+  invalidCredentials = false;
+
+  login(){
+
+  }
+
+}
